242169e8e14317d5054c8fd50f55bf9ddbecb0d6
lukaas33 <lucasvanosenbruggen@gmail.com>
2023-02-12 16:08:07 +0100

    Implemented reasonable method of characterizing overlap by using subsequence

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ def print_seq_diff(sequence1, sequence2):
     for i,s in enumerate(difference):
         if s[0]==' ': continue
         elif s[0]=='-':
-            print(u'Delete "{}" from position {}'.format(s[-1],i+1))
+            print(u'Delete "{}" on relative position {}'.format(s[-1],i+1))
         elif s[0]=='+':
-            print(u'Add "{}" to position {}'.format(s[-1],i+1)) 
+            print(u'Insert "{}" on relative position {}'.format(s[-1],i+1)) 
 
 def va_generate_subsets(variants):
     """Generate all subsets (superset) of a variant set"""
@@ -33,11 +33,13 @@ def va_minimal_overlapping_allele(reference_sequence, lhs_allele, rhs_allele):
     
     Uses top-down linear search.
 
-    WARNING: worse case complexity is not good
+    WARNING: worse case complexity is not good for bigger variants
     """
     # QUESTION is it valid to have both sequences be the same length
     # QUESTION binary search possible instead of linear search?
-    # Find minimal slice of left allele still overlapping with right allele
+    # TODO change approach to work for more downstream overlapping variants (since area would now get large for no reason)
+    #       use sliding and extending window?
+    # Start extending alleles from left until an fragment with overlap is found
     if lhs_allele.start != rhs_allele.start or lhs_allele.end != rhs_allele.end:
         # TODO solve for lengths not being equal
         raise Exception("Lengths not the same")
@@ -49,14 +51,15 @@ def va_minimal_overlapping_allele(reference_sequence, lhs_allele, rhs_allele):
         if relation == va.Relation.OVERLAP:
             min_variant_l, min_variant_r =  sub_variant_l, sub_variant_r
             break
+    # See if it can be reduced more by making it smaller while overlapping
     lhs_allele, rhs_allele = min_variant_l, min_variant_r
     for slice_l in range(0, lhs_allele.end - lhs_allele.start):
         subsequence_l, subsequence_r = lhs_allele.sequence[slice_l:], rhs_allele.sequence[slice_l:]
         sub_variant_l, sub_variant_r = va.Variant(lhs_allele.start + slice_l, lhs_allele.end, subsequence_l), va.Variant(lhs_allele.start + slice_l, lhs_allele.end, subsequence_r)
         relation = va.compare(reference_sequence, {sub_variant_l}, {sub_variant_r})
-        if relation == va.Relation.OVERLAP:
-            min_variant_l, min_variant_r =  sub_variant_l, sub_variant_r
+        if relation != va.Relation.OVERLAP: # Starts as overlapping, continue until it isn't anymore
             break
+        min_variant_l, min_variant_r =  sub_variant_l, sub_variant_r
     lhs_allele, rhs_allele = min_variant_l, min_variant_r
     return lhs_allele, rhs_allele
 
@@ -65,17 +68,15 @@ def va_characterize_overlap(reference_sequence, lhs, rhs):
     
     This extends the variant algebra which detects but does not characterize the overlap.
     Overlap cannot always be explained by shared variant positions (simple set overlap).
+
+    WARNING: not efficient, only works for very small overlapping areas
     """
-    # TODO integrate in va?
-    # BUG: not efficient enough, memory error
     # QUESTION is the atomics of the allele equal to the (combination of) atomics of all variants
-    # Combine variants into allele
+    # Combine variants into allele (supremal representation)
     lhs_allele = combine_variants(lhs, reference_sequence)
     rhs_allele = combine_variants(rhs, reference_sequence)
     # Reduce allele size to the minimal size with overlap to make the atomics more manageable
     lhs_allele, rhs_allele = va_minimal_overlapping_allele(reference_sequence, lhs_allele, rhs_allele)
-    print(lhs_allele, rhs_allele)
-    exit()
     # Find the set of minimal representations of both alleles
     l_minset = set([tuple(minrep) for minrep in lhs_allele.atomics()])
     r_minset = set([tuple(minrep) for minrep in rhs_allele.atomics()])
@@ -86,11 +87,15 @@ def va_characterize_overlap(reference_sequence, lhs, rhs):
     return l_only, shared, r_only
 
 def test_coreallele_containment(corealleles, suballeles, reference_sequence, coreallele_name):
-    """Test if the coreallele is contained in each suballele."""
-    # All information associated with the allele of interest
-    coreallele = corealleles[coreallele_name]
+    """Test if the coreallele is contained in each suballele.
+    
+    By definition each suballele should contain the coreallele.
+    If this is not the case there is an inconsistency.
+    This is detected and then characterized.
+    """
     # Get the variants contained in the sub- and core allele as HGVS notation
-    # QUESTION why doesn't name match position
+    # QUESTION why doesn't name always match position
+    coreallele = corealleles[coreallele_name]
     coreallele_variants = [variant["position"] for variant in coreallele["variants"]]
     for suballele in suballeles:
         suballele_variants = [variant["position"] for variant in suballele["variants"]]
@@ -102,13 +107,15 @@ def test_coreallele_containment(corealleles, suballeles, reference_sequence, cor
             relation = va.compare(reference_sequence, s_coreallele_variants, s_suballele_variants)
         except Exception as e:
             raise ValueError(f"Could not compare variants {coreallele_variants} with {suballele_variants}")
-        # Expect containment or equivalence
+        # Expect containment or equivalence for core and suballele
+        # QUESTION is an equivalence between a sub and core allele inconsistent?
         if relation not in (va.Relation.EQUIVALENT, va.Relation.IS_CONTAINED):
             # For unexpected relationships the overlap should be characterized
-            # QUESTION is an equivalence between a sub and core allele inconsistent?
             print(f"{coreallele['alleleName']}: Unexpected relationship {relation} with suballele {suballele['alleleName']}:")
             only_core, shared, only_sub = va_characterize_overlap(reference_sequence, s_coreallele_variants, s_suballele_variants)
-            print(f"\t{only_core} is found in the core but not in the suballele")
+            print(f"\tMinimal representations found in the core but not in the suballele:")
+            for min_repr in only_core:
+                print(f"\t{va.variants.to_hgvs(min_repr, reference_sequence)}")
 
 def main():
     # Get the reference sequence relevant for the (current) gene of interest
